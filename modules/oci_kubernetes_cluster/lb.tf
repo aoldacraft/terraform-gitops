1e009bcb8393be15124e4fd8290bdc84fbed0039
minshigee <minshigee@gmail.com>
2023-05-23 21:27:15 +0900

    lb-update

--- modules/oci_kubernetes_cluster/lb.tf
+++ modules/oci_kubernetes_cluster/lb.tf
@@ -7,7 +7,7 @@ resource "oci_network_load_balancer_network_load_balancer" "net_ingress_main" {
   compartment_id = oci_identity_compartment.project.id
   display_name = var.lb_name
   subnet_id = oci_core_subnet.public.id
-  is_private = var.env == "prod" ? false : true
+  is_private = false
   is_preserve_source_destination = true
 }
 
@@ -15,11 +15,12 @@ resource "oci_network_load_balancer_network_load_balancer" "net_ingress_main" {
 Listener
 */
 resource "oci_network_load_balancer_listener" "ingress_main" {
+  count = length(var.lb_listening_ports.*)
   #Required
   default_backend_set_name = oci_network_load_balancer_backend_set.ingress_main.name
   name = "main"
   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.net_ingress_main.id
-  port = var.lb_listening_ports[0]
+  port = var.lb_listening_ports[count.index]
   protocol = "TCP_AND_UDP"
 }
 
@@ -27,7 +28,8 @@ resource "oci_network_load_balancer_listener" "ingress_main" {
 Backend
 */
 resource "oci_network_load_balancer_backend_set" "ingress_main" {
-  name = "backend"
+  count = length(var.lb_listening_ports.*)
+  name = "backend-${count.index}"
   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.net_ingress_main.id
   policy = "FIVE_TUPLE"
   health_checker {
